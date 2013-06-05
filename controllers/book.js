874f2432ec9536a13dbd96768d246989667df6e9
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-06-05 17:48:32 -0400

    controller refactor

--- controllers/book.js
+++ controllers/book.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function BookCtrl($scope) {
-  $scope.books = [{ text: 'The UNIX Programming Environment' }];
+var zenodotus = angular.module('zenodotus', ['ngResource']);
 
+zenodotus.factory('Book', ['$resource', function($resource) {
+  return $resource('/books/:id', {id: '@id'});
+}]);
+
+zenodotus.controller('BookCtrl', ['$scope', 'Book', function($scope, Book) {
+  $scope.books = Book.query();
+
+  $scope.search = function(query) {
+    // !!! Unsafe !!!
+    query = JSON.stringify(query);
+
+    Book.query({query: query}, function(books) {
+      $scope.books = books;
+    });
+  };
+}]);
+
+function BookCtrl($scope) {
   $scope.count = function() {
     var count = 0;
     angular.forEach($scope.books, function() {
