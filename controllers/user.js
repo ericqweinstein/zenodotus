060166db8bc5e7e3487cef32a2d25341b4a58837
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-06-06 15:59:48 -0400

    beginnings of user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,2 +1,12 @@
 'use strict';
 
+var zenodotus = angular.module('zenodotus', ['ngResource']);
+
+zenodotus.factory('User', ['$resource', function($resource) {
+  return $resource('/users/:id', {id: '@id'});
+}]);
+
+zenodotus.controller('UserCtrl', ['$scope', 'User', function($scope, User) {
+  // Controller magicks TK
+}]);
+
