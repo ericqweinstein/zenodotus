469f8313fd7ac6249c806e66d40c00eb24e7ede0
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-06-11 20:02:22 -0400

    external links open in new windows

--- public/js/custom.js
+++ public/js/custom.js
@@ -48,3 +48,20 @@ $(function() {
   });
 });
 
+/*
+ * Open all external links in new windows.
+ */
+
+$(function() {
+  $('a').each(function() {
+    var a = new RegExp('/' + window.location.host + '/');
+    if (!a.test(this.href)) {
+      $(this).click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        window.open(this.href, '_blank');
+      });
+    }
+  });
+});
+
