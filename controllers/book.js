4d3e1814fd797d2957752b261f8a66af686c33ee
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-06-17 13:00:37 -0400

    first stab at Angular AJAX

--- controllers/book.js
+++ controllers/book.js
@@ -26,19 +26,35 @@ zenodotus.controller('BookCtrl', ['$scope', 'Book', function($scope, Book) {
     var pluralization = count == 1 ? " title" : " titles";
     return count + pluralization;
   };
+}]);
 
-  // May be deprecated
-  $scope.tokenizeTitle = function(title) {
-    var forbiddenTokens = /[-:\/?!.;]/g;
-    return title.toLowerCase().replace(forbiddenTokens, '')
-                              .replace(/\s+/g, '-');
+// Angular AJAX controller for retrieving
+// book data via the Google Books API
+//
+// (Using a test ISBN for now)
+function BookFetchCtrl($scope, $http) {
+  $scope.method = 'JSONP'
+, $scope.url    = 'https://www.googleapis.com/books/v1/volumes?q=isbn:9780139376818?callback=alertData'
+, $scope.fetch  = function() {
+    $scope.bookDescription = null
+  , $scope.bookCoverLink   = null
+  , $scope.bookInfoLink    = null;
+
+    $http({ method: $scope.method, url: $scope.url }).
+      success(function(data, status) {
+        $scope.bookDescription = data['items'][0]['volumeInfo']['description'];
+        $scope.bookCoverLink   = data['items'][0]['volumeInfo']['imageLinks']['thumbnail'];
+        $scope.bookInfoLink    = data['items'][0]['volumeInfo']['infoLink'];
+        console.log(data);
+      }).
+      error(function(data, status) {
+        $scope.data   = data || 'Request failed.';
+        $scope.status = status;
+        console.log(data);
+    });
   };
 
-  // Attributes for the title detail view
-  $scope.currentTitleIndex;
-  $scope.bookDescription;
-  $scope.bookCoverLink;
-  $scope.bookInfoLink;
+  $scope.alertData = function(data) { alert(data); }
 
   // Get the current title index for the title detail view
   //
@@ -49,17 +65,5 @@ zenodotus.controller('BookCtrl', ['$scope', 'Book', function($scope, Book) {
       $scope.currentTitleIndex = +self.href.split('/')[3];
     });
   });
-
-  // AJAX request for book metadata
-  // via the Google Books API
-  jQuery(document).on('click', '.book-title', function() {
-    $scope.$apply(function() {
-      var response = jQuery.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + $('.isbn').html(), function() {
-        }).done(function(data) { $scope.bookDescription = data['items'][0]['volumeInfo']['description'];
-                                 $scope.bookCoverLink   = data['items'][0]['volumeInfo']['imageLinks']['thumbnail'];
-                                 $scope.bookInfoLink = data['items'][0]['volumeInfo']['infoLink']; })
-          .fail(function() { console.log('An error occurred.'); });
-    });
-  });
-}]);
+}
 
